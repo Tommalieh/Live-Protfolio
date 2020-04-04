6087962e2ca07f759786c8fc6985c3b5480170e4
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-04 21:43:47 +0300

    Ready to deploy

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+    response.status(200).send('Hello');
+});
+
+app.get('/data', (request, response) => {
+    let data = [{name: 'JavaScript'}, {name: 'python'}, {name: 'C#'}];
+    response.status(200).json(data);
+});
+
+app.get('*', (request, response) => {
+    response.status(200).json('404 Not Found');
+});
+
+
+app.listen(PORT, () => console.log(`listening to the ${PORT}`));
